ce23bcf2c509a40648b659ee48e5c150e99296ed
fadhlyrafi <rafiabang@gmail.com>
2024-07-09 21:44:42 +0700

    Page Infografis
    - Tailwind Installation
    - svg assets
    - Temporary route /infografis
    - Infografis page + responsive flex wrap
    - Without navbar and footter, still waiting for components

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/views/infografis.blade.php"
+  ],
+  theme: {
+    extend: {},
+    fontFamily: {
+      poppins: ['"Poppins"', "sans-serif"],
+    },
+  },
+  plugins: [],
+}
+
